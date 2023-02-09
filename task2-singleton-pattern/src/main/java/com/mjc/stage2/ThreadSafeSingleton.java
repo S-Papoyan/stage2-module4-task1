3236c2552e7dff71e0c594917a7f7a7f4d9d41c6
Sergey <spapoyan1987@gmail.com>
2023-02-09 23:38:36 +0400

    commit

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,14 +1,19 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
+
     private static volatile ThreadSafeSingleton instance;
+    private static final Object MONITOR = new Object();
+
 
     private ThreadSafeSingleton() {
     }
 
     public static ThreadSafeSingleton getInstance(){
         if(instance == null){
-            instance = new ThreadSafeSingleton();
+            synchronized (MONITOR) {
+                instance = new ThreadSafeSingleton();
+            }
         }
         return instance;
     }
